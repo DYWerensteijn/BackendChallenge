3550124a6ced270890b6a13438bf415779c3c9df
DYWerensteijn <99057620@mydavinci.nl>
2022-11-21 11:39:53 +0100

    built a head function where you can dynamically set the title

---
+++ parts/head.php
@@ -0,0 +1,16 @@
+<?php
+
+function Head($page_title){
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <title><?php echo $page_title ?></title>
+</head>
+<?php
+}
